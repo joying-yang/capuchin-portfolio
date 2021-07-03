2377c2dfc66ee7da6c1aa0666b0e7c9314b43c9e
Cloud User <centos@ip-172-31-7-0.ca-central-1.compute.internal>
2021-07-03 00:05:33 +0000

    mmmm

--- app/__init__.py
+++ app/__init__.py
@@ -2,15 +2,9 @@ import os
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask import Flask, render_template, send_from_directory, request
 from dotenv import load_dotenv
-<<<<<<< HEAD
-from . import db
-from werkzeug.security import generate_password_hash, check_password_hash
-from app.db import get_db
-=======
 # from . import db
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
->>>>>>> ad3ea873e31ecd867b2da84a88b845d8b88fdc3a
 
 load_dotenv()
 
@@ -66,56 +60,49 @@ def projects():
 def health():
     return '', 200
 
-@app.route('/login', methods=('GET', 'POST'))
-def login():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
-        db = get_db()
-        error = None
-        user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username,)
-        ).fetchone()
-
-        if user is None:
-            error = 'Incorrect username.'
-        elif not check_password_hash(user['password'], password):
-            error = 'Incorrect password.'
-
-        if error is None:
-            return "Login Successful", 200 
-        else:
-            return error, 418
-    
-    ## TODO: Return a login page
-    return render_template('login.html', title="Login", url=os.getenv("URL"))
-
 @app.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        db = get_db()
         error = None
 
         if not username:
             error = 'Username is required.'
         elif not password:
             error = 'Password is required.'
-        elif db.execute(
-            'SELECT id FROM user WHERE username = ?', (username,)
-        ).fetchone() is not None:
+        elif UserModel.query.filter_by(username=username).first() is not None:
             error = f"User {username} is already registered."
 
         if error is None:
-            db.execute(
-                'INSERT INTO user (username, password) VALUES (?, ?)',
-                (username, generate_password_hash(password))
-            )
-            db.commit()
+            new_user = UserModel(username, generate_password_hash(password))
+            db.session.add(new_user)
+            db.session.commit()
             return f"User {username} created successfully"
         else:
             return error, 418
 
-    ## TODO: Return a restister page
+    # TODO: Return a restister page
     return render_template('register.html', title="Register", url=os.getenv("URL"))
+
+
+@app.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        error = None
+        user = UserModel.query.filter_by(username=username).first()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user.password, password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            return "Login Successful", 200
+        else:
+            return error, 418
+
+    # TODO: Return a login page
+    return render_template('login.html', title="Login", url=os.getenv("URL"))

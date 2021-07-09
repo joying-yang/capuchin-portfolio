732efafb22a2ad1af196c989183b366d0339e108
joying-yang <joying0614@gmail.com>
2021-07-08 17:37:52 -0700

    edit deploy.yml

--- prod-test.sh
+++ prod-test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-output=$(curl -s 'https://wek1-mlh-demo.duckdns.org')
+output=$(curl -s 'https://week1-mlh-demo.duckdns.org')
 if [[ $? -ne 0 ]]
 then
   exit 1
 fi
 
-output=$(curl -s 'https://wek1-mlh-demo.duckdns.org/register')
+output=$(curl -s 'https://week1-mlh-demo.duckdns.org/register')
 if [[ $? -ne 0 ]]
 then
   exit 1

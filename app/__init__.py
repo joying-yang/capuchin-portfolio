32b07bc8a65da703e5028ec2224d4fd89ea1421e
Lint Action <lint-action@samuelmeuli.com>
2021-07-09 01:11:18 +0000

    Fix code style issues with Black

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ import os
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask import Flask, render_template, send_from_directory, request
 from dotenv import load_dotenv
+
 # from . import db
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -9,14 +10,17 @@ from flask_migrate import Migrate
 load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{table}'.format(
-    user=os.getenv('POSTGRES_USER'),
-    passwd=os.getenv('POSTGRES_PASSWORD'),
-    host=os.getenv('POSTGRES_HOST'),
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{table}".format(
+    user=os.getenv("POSTGRES_USER"),
+    passwd=os.getenv("POSTGRES_PASSWORD"),
+    host=os.getenv("POSTGRES_HOST"),
     port=5432,
-    table=os.getenv('POSTGRES_DB'))
+    table=os.getenv("POSTGRES_DB"),
+)
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 
 db = SQLAlchemy(app)
@@ -25,7 +29,7 @@ migrate = Migrate(app, db)
 
 # db.init_app(app)
 class UserModel(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     username = db.Column(db.String(), primary_key=True)
     password = db.Column(db.String())
@@ -36,41 +40,47 @@ class UserModel(db.Model):
 
     def __repr__(self):
         return f"<User {self.username}>"
+
+
 ## Rest of the file
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    return render_template("index.html", title="MLH Fellow", url=os.getenv("URL"))
+
 
 # @app.route('/about')
 # def about():
 #     return render_template('about.html', title="About", url=os.getenv("URL"))<
 
 
-@app.route('/contact')
+@app.route("/contact")
 def contact():
-    return render_template('contact.html', title="Contact", url=os.getenv("URL"))
+    return render_template("contact.html", title="Contact", url=os.getenv("URL"))
 
 
-@app.route('/projects')
+@app.route("/projects")
 def projects():
-    return render_template('projects.html', title="Projects", url=os.getenv("URL"))
+    return render_template("projects.html", title="Projects", url=os.getenv("URL"))
 
-@app.route('/health')
+
+@app.route("/health")
 def health():
-    return '', 200
+    return "", 200
 
-@app.route('/register', methods=('GET', 'POST'))
+
+@app.route("/register", methods=("GET", "POST"))
 def register():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
         error = None
 
         if not username:
-            error = 'Username is required.'
+            error = "Username is required."
         elif not password:
-            error = 'Password is required.'
+            error = "Password is required."
         elif UserModel.query.filter_by(username=username).first() is not None:
             error = f"User {username} is already registered."
 
@@ -83,21 +93,21 @@ def register():
             return error, 418
 
     # TODO: Return a restister page
-    return render_template('register.html', title="Register", url=os.getenv("URL"))
+    return render_template("register.html", title="Register", url=os.getenv("URL"))
 
 
-@app.route('/login', methods=('GET', 'POST'))
+@app.route("/login", methods=("GET", "POST"))
 def login():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
         error = None
         user = UserModel.query.filter_by(username=username).first()
 
         if user is None:
-            error = 'Incorrect username.'
+            error = "Incorrect username."
         elif not check_password_hash(user.password, password):
-            error = 'Incorrect password.'
+            error = "Incorrect password."
 
         if error is None:
             return "Login Successful", 200
@@ -105,4 +115,4 @@ def login():
             return error, 418
 
     # TODO: Return a login page
-    return render_template('login.html', title="Login", url=os.getenv("URL"))
+    return render_template("login.html", title="Login", url=os.getenv("URL"))
